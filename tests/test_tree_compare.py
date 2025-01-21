5b0f2a8cf76a886406d07e38a6cb1cc3e4fb39ac
Sarah Walling-Bell <sarah.wallingbell@alleninstitute.org>
2025-01-21 15:46:06 -0800

    add: reflexivity test

--- tests/test_tree_compare.py
+++ tests/test_tree_compare.py
@@ -10,25 +10,28 @@ class TestTreeCompare(unittest.TestCase):
     #TODO add test that a tree compared to itself has distance == 0
 
     def setUp(self):
-        self.tree1 = os.path.join(os.path.dirname(__file__), 'test_data', '601506507.swc')
-        self.tree2 = os.path.join(os.path.dirname(__file__), 'test_data', '898703349.swc')
+        tree1 = os.path.join(os.path.dirname(__file__), 'test_data', 'swc_dir', '601506507.swc')
+        tree2 = os.path.join(os.path.dirname(__file__), 'test_data', 'ref_dir', '898703349.swc')
         self.valid_set_dict = os.path.join(str(files('tree_comparison') / "data"), 'validSet_mouse_inh_viz_ctx.json')
 
         self.test_cases = [
-            {"params": {"compartments": [AXON],           "simFunc": "convex", "maxDepth": 2, "orientation": 0},      "expected_distance": 6516.7242},
-            {"params": {"compartments": [BASAL_DENDRITE], "simFunc": "convex", "maxDepth": 2, "orientation": 0},      "expected_distance": 4340.9097},
-            {"params": {"compartments": [BASAL_DENDRITE], "simFunc": "convex", "maxDepth": 2, "orientation": 180},    "expected_distance": 4624.9772},
-            {"params": {"compartments": [BASAL_DENDRITE], "simFunc": "convex", "maxDepth": 1, "orientation": 0},      "expected_distance": 4362.4997},
-            {"params": {"compartments": [BASAL_DENDRITE], "simFunc": "length", "maxDepth": 2, "orientation": 0},      "expected_distance": 2921.645},
-            {"params": {"compartments": [BASAL_DENDRITE], "simFunc": "length", "maxDepth": 1, "orientation": 0},      "expected_distance": 3060.1586},
+            {"params": {"tree1": tree1, "tree2": tree2, "compartments": [AXON],           "simFunc": "convex", "maxDepth": 2, "orientation": 0},      "expected_distance": 6516.7242},
+            {"params": {"tree1": tree1, "tree2": tree2, "compartments": [BASAL_DENDRITE], "simFunc": "convex", "maxDepth": 2, "orientation": 0},      "expected_distance": 4340.9097},
+            {"params": {"tree1": tree1, "tree2": tree2, "compartments": [BASAL_DENDRITE], "simFunc": "convex", "maxDepth": 2, "orientation": 180},    "expected_distance": 4624.9772},
+            {"params": {"tree1": tree1, "tree2": tree2, "compartments": [BASAL_DENDRITE], "simFunc": "convex", "maxDepth": 1, "orientation": 0},      "expected_distance": 4362.4997},
+            {"params": {"tree1": tree1, "tree2": tree2, "compartments": [BASAL_DENDRITE], "simFunc": "length", "maxDepth": 2, "orientation": 0},      "expected_distance": 2921.645},
+            {"params": {"tree1": tree1, "tree2": tree2, "compartments": [BASAL_DENDRITE], "simFunc": "length", "maxDepth": 1, "orientation": 0},      "expected_distance": 3060.1586},
+            {"params": {"tree1": tree1, "tree2": tree1, "compartments": [AXON],           "simFunc": "convex", "maxDepth": 2, "orientation": 0},      "expected_distance": 0},
+            {"params": {"tree1": tree1, "tree2": tree1, "compartments": [BASAL_DENDRITE], "simFunc": "length", "maxDepth": 2, "orientation": 0},      "expected_distance": 0},
+
         ]
 
     def test_compare_two_trees(self):
         for case in self.test_cases:
             with self.subTest(**case["params"]):
                     distance, *_ = compare_two_trees(
-                        swc_file_1=self.tree1,
-                        swc_file_2=self.tree2,
+                        swc_file_1=case["params"]["tree1"],
+                        swc_file_2=case["params"]["tree2"],
                         compartments=case["params"]["compartments"],
                         simFunc=case["params"]["simFunc"],
                         maxDepth=case["params"]["maxDepth"],
